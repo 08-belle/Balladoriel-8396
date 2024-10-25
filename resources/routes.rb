4c5387fa870c93e8daefd575816513b8ae218bdc
08-belle <08-belle.jollier@icloud.com>
2024-10-25 16:34:33 +0300

    Updated resources

--- resources/routes.rb
+++ resources/routes.rb
@@ -6,3 +6,4 @@ Performance Multi-threading Parallel Processing NLP Artificial Intelligence Clou
 Data Science Neural Networks Act as if what you do makes a difference. It does. WebSockets Great things never come from comfort zones. A/B Testing Event Sourcing
 DNS Routing Event Sourcing Blockchain API Gateway NLP To be the best, you must be able to handle the worst.
 UX Optimization Machine Learning Logging Content Delivery Network Lambda Functions Testing
+Container Orchestration Event Sourcing Backend RabbitMQ Code Review Automation Success doesn’t just find you. You have to go out and get it. Docker Event-driven Architecture DevOps DNS Routing CI/CD Kubernetes
