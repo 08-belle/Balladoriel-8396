c14b75d05f453f7c280a3b5aa7dbedf2346938ba
08-belle <08-belle.jollier@icloud.com>
2024-11-03 16:34:30 +0300

    Updated resources

--- resources/database.sql
+++ resources/database.sql
@@ -3,3 +3,4 @@ Automation Kubernetes Lambda Functions Sharding Big Data React Security
 API Gateway OAuth Success is not in what you have, but who you are. Artificial Intelligence Code Review Parallel Processing Security I find that the harder I work, the more luck I seem to have. Message Queue
 React Serverless Data Pipeline Virtual Machines Load Balancing API Gateway Cloud Functions Act as if what you do makes a difference. It does. GraphQL Agile Big Data Do what you love and the money will follow. Server Response DevOps Monitoring
 Difficult roads often lead to beautiful destinations. Great things never come from comfort zones. Don't be afraid to give up the good to go for the great. Edge Computing Event-driven Architecture RabbitMQ A/B Testing Lambda Functions Event Sourcing Deployment Neural Networks
+DevOps Docker Containerization Container Orchestration Message Queue
