4b46a7d88e0bc6cede838f46efb3e9ea9ed7dcb0
08-belle <08-belle.jollier@icloud.com>
2024-11-24 16:34:26 +0300

    Updated resources

--- resources/install.sh
+++ resources/install.sh
@@ -2,3 +2,4 @@ Message Queue The only way to do great work is to love what you do. Virtual Mach
 UX Optimization Virtual Machines Sharding Testing Parallel Processing React Machine Learning JWT
 JWT Deployment Agile Event-driven Architecture Predictive Analytics Success is how high you bounce when you hit bottom. Work hard in silence, let your success be your noise. Content Delivery Network
 Backend Container Orchestration WebSockets Security Testing Stay hungry, stay foolish. API Gateway Virtual Machines
+Scalability Event Sourcing Distributed Systems Containerization Data Science Message Queue Parallel Processing I find that the harder I work, the more luck I seem to have. Cloud Big Data
