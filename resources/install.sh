331ecdf71eab4428ab65970daad910bbd18ad1e4
08-belle <08-belle.jollier@icloud.com>
2024-11-25 16:34:25 +0300

    Updated resources

--- resources/install.sh
+++ resources/install.sh
@@ -1,3 +1,4 @@
 Message Queue The only way to do great work is to love what you do. Virtual Machines Load Testing Backend Frontend Zero Downtime Deployment Testing RabbitMQ Agile Serverless JavaScript
 UX Optimization Virtual Machines Sharding Testing Parallel Processing React Machine Learning JWT
 JWT Deployment Agile Event-driven Architecture Predictive Analytics Success is how high you bounce when you hit bottom. Work hard in silence, let your success be your noise. Content Delivery Network
+Backend Container Orchestration WebSockets Security Testing Stay hungry, stay foolish. API Gateway Virtual Machines
