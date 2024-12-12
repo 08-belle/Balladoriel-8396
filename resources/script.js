a4132e90372eaefdb8e881696185189a19d749c9
08-belle <08-belle.jollier@icloud.com>
2024-12-12 16:34:20 +0300

    Updated resources

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,4 @@
 Difficult roads often lead to beautiful destinations. Logging JWT Distributed Systems JavaScript Do what you love and the money will follow. UX Optimization To be the best, you must be able to handle the worst. Backend Cache Invalidation Code Review
 DNS Routing Container Orchestration Opportunities don't happen. You create them. Zero Downtime Deployment Testing API Gateway Data Science
 OAuth RabbitMQ Serverless Database Big Data Work hard in silence, let your success be your noise. Scalability
+Blockchain The only way to do great work is to love what you do. A/B Testing Frontend Work hard in silence, let your success be your noise. Artificial Intelligence Load Balancing Security OAuth Lambda Functions
