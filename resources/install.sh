70c4dc8ec4efa283bedc213a1e1e86d18674a05f
08-belle <08-belle.jollier@icloud.com>
2025-02-11 16:34:10 +0300

    Updated resources

---
+++ resources/install.sh
@@ -0,0 +1,1 @@
+Message Queue The only way to do great work is to love what you do. Virtual Machines Load Testing Backend Frontend Zero Downtime Deployment Testing RabbitMQ Agile Serverless JavaScript
